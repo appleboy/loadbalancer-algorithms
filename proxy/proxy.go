43bd262d242792059aef9152d8efe9179c92a18a
appleboy <appleboy.tw@gmail.com>
2024-09-01 07:07:12 +0800

    feat: implement reverse proxy functionality and tests
    - Introduce a new file for the proxy implementation
    - Add a Proxy struct for handling reverse proxy functionality
    - Implement ServeHTTP method for processing incoming HTTP requests
    - Include a method to retrieve the current loading of the proxy
    - Create a new test file for the proxy functionality
    
    Signed-off-by: appleboy <appleboy.tw@gmail.com>

---
+++ proxy/proxy.go
@@ -0,0 +1,33 @@
+package proxy
+
+import (
+	"net/http"
+	"net/http/httputil"
+	"sync/atomic"
+)
+
+var value int32 = -1
+
+// Proxy represents a reverse proxy for load balancing algorithms.
+type Proxy struct {
+	proxy   *httputil.ReverseProxy
+	loading uint32
+}
+
+// ServeHTTP handles the incoming HTTP request and forwards it to the underlying proxy server.
+// It increments the load counter by 1 before forwarding the request and decrements it by the given value after the request is processed.
+// This method is part of the Proxy struct and implements the http.Handler interface.
+//
+// Parameters:
+// - w: The http.ResponseWriter used to write the response back to the client.
+// - r: The http.Request representing the incoming request.
+func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	atomic.AddUint32(&p.loading, 1)
+	defer atomic.AddUint32(&p.loading, uint32(value))
+	p.proxy.ServeHTTP(w, r)
+}
+
+// GetLoading returns the current loading of the proxy.
+func (p *Proxy) GetLoading() uint32 {
+	return atomic.LoadUint32(&p.loading)
+}
