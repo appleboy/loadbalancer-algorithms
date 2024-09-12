9f5e723880e2e13c84cf9179416c2650cc94015e
appleboy <appleboy.tw@gmail.com>
2024-09-13 07:54:29 +0800

    test: enhance health check testing framework
    - Add a new test function for checking proxy health
    - Introduce a series of test cases for different health check scenarios
    - Validate the behavior of the health check based on success and failure thresholds
    - Ensure the health check correctly reflects availability status after various conditions
    
    Signed-off-by: appleboy <appleboy.tw@gmail.com>

--- proxy/health/health_test.go
+++ proxy/health/health_test.go
@@ -159,3 +159,85 @@ func TestDefaultDNSCheck(t *testing.T) {
 		})
 	}
 }
+
+func TestProxyHealth_checkHealth(t *testing.T) {
+	tests := []struct {
+		name             string
+		check            Check
+		successThreshold int
+		failureThreshold int
+		failureCount     int
+		wantAvailable    bool
+		isAvailable      bool
+	}{
+		{
+			name: "Health check passes",
+			check: func(addr *url.URL) bool {
+				return true
+			},
+			successThreshold: defaultSuccessThreshold,
+			failureThreshold: defaultFailureThreshold,
+			wantAvailable:    true,
+			failureCount:     0,
+		},
+		{
+			name: "Health check fails",
+			check: func(addr *url.URL) bool {
+				return false
+			},
+			successThreshold: defaultSuccessThreshold,
+			failureThreshold: 1,
+			wantAvailable:    false,
+			failureCount:     0,
+		},
+		{
+			name: "Health check passes after failures",
+			check: func(addr *url.URL) bool {
+				if addr.String() == "http://example.com" {
+					return false
+				}
+				return true
+			},
+			successThreshold: defaultSuccessThreshold,
+			failureThreshold: defaultFailureThreshold,
+			wantAvailable:    true,
+			isAvailable:      true,
+			failureCount:     1,
+		},
+		{
+			name: "failing health check after reaching failure threshold",
+			check: func(addr *url.URL) bool {
+				if addr.String() == "http://example.com" {
+					return false
+				}
+				return true
+			},
+			successThreshold: defaultSuccessThreshold,
+			failureThreshold: defaultFailureThreshold,
+			wantAvailable:    false,
+			isAvailable:      true,
+			failureCount:     2,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			origin, _ := url.Parse("http://example.com")
+			h := &ProxyHealth{
+				origin:           origin,
+				check:            tt.check,
+				successThreshold: tt.successThreshold,
+				failureThreshold: tt.failureThreshold,
+				cancel:           make(chan struct{}),
+				failureCount:     tt.failureCount,
+				isAvailable:      tt.isAvailable,
+			}
+
+			h.checkHealth()
+
+			if h.isAvailable != tt.wantAvailable {
+				t.Errorf("checkHealth() = %v, want %v", h.isAvailable, tt.wantAvailable)
+			}
+		})
+	}
+}
